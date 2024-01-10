55166b3be05b2f03bdb65cd6ffd746c9302632dc
Saddii <81620948+Saddii@users.noreply.github.com>
2024-01-10 23:42:18 +0100

    Update Neural_network.py

--- Neural_network.py
+++ Neural_network.py
@@ -1,108 +1,77 @@
 import numpy as np
-from matplotlib import pyplot
-import pickle
 import pathlib
 
-def function(x): #funkcja aktywacji
-    return 1/(1+np.exp(-x))
-def fun_deriv(x):
-    return function(x)*(1-function(x))
-fun = np.vectorize(function)
-fun_d = np.vectorize(fun_deriv)
-c=0.01
-with np.load(f"{pathlib.Path(__file__).parent.absolute()}/mnist.npz") as f:
-    train_X,train_y= f["x_train"], f["y_train"]
-    test_X,test_y=f["x_test"],f["y_test"]
+class NeuralNetwork:
+    def __init__(self,learning_rate =0.01):
+        self.learning_rate = learning_rate
 
-train_X =train_X.astype("float32") / 255
-np.reshape(train_X, (train_X.shape[0], train_X.shape[1] * train_X.shape[2]))
-train_y = np.eye(10)[train_y]
+        with np.load(f"{pathlib.Path(__file__).parent.absolute()}/mnist.npz") as f:
+            self.train_X,self.train_y= f["x_train"]/255, f["y_train"]
+            self.test_X,self.test_y=f["x_test"],f["y_test"]
 
+    def fit(self,neurons_in_layer=(784,20,10),Epochs=3,c=0.01):
+        
+        self.train_y = np.eye(10)[self.train_y]
 
-with open('W_1.pkl', 'rb') as f:
-    W_1 = pickle.load(f)
-with open('W_2.pkl', 'rb') as f:
-    W_2 = pickle.load(f)
+        layers = len(neurons_in_layer)
+        obs_num, n , m = self.train_X.shape  #60k Observations  28x28 matrix
+        X = self.train_X.reshape(obs_num, n*m,1) #60k vectors len = 784
+        W = [] 
 
-# print(train_X.shape,train_y.shape)
-# W_1 = np.random.uniform(-0.5,0.5,(100,785))
-# W_2 = np.random.uniform(-0.5,0.5,(10,101))
-
-X = train_X.reshape(60000,784,1)
-
-def learn(W_1,W_2,X):
-    prawidlowe = 0 
-    i=0
-    for j in range(3):
-        for i in range(60000):
-            X_1 = np.insert(X[i],0,1)
-            net_1 = W_1@X_1
-            X_2 = 1/(1+np.exp(-net_1))
-            X_2 = np.insert(X_2,0,1)
-            net_2 = W_2@X_2
-            a = 1/(1+np.exp(-net_2))
-            prawidlowe+= int(np.argmax(a)==np.argmax(train_y[i]))
-            # if np.argmax(a)!=np.argmax(train_y[i]):
+        for w in range(layers-1):
+            W.append(np.random.uniform(-0.5,0.5,(neurons_in_layer[w+1],neurons_in_layer[w]+1))) # +1 ->bias
+        
+        correct = 0
+        for j in range(Epochs):
+            for i in range(obs_num):
                 
-            #     img = train_X[i]
-            #     pyplot.imshow(img.reshape(28,28),cmap="Greys")
-            #     pyplot.show()
+                #forwardporopagation
+                X_0 = np.insert(X[i],0,1) #+ bias 
+                net_0 = W[0]@X_0 #vals of neurons before activation in first layer
+                
+                X_1 = 1/(1+np.exp(-net_0)) #val of neurons after activation in first layer
+                X_1 = np.insert(X_1,0,1) #+ bias
+                net_1 = W[1]@X_1 #vals of neurons before activation in output layer
+                a = 1/(1+np.exp(-net_1)) #activation in output layer
+                correct += int(np.argmax(a)==np.argmax(self.train_y[i]))
 
+                #backpropagation
+                #loss = (a-self.train_y)^2
+                L = a - self.train_y[i] # Loss deriv
+                d_1= L
+                L_W_1 = L.reshape(10,1) @X_1.reshape(1,-1)
+                W[1] += -c*L_W_1
+                L_X_1 = (W[1]).T @ d_1
 
-            
-            L= a-train_y[i]
-            d_2= L
-            L_W_2 = L.reshape(10,1) @(X_2.reshape(1,X_2.shape[0]))
-            W_2 += -c*L_W_2
-            L_X_2 = (W_2).T @ d_2
+                L_a_0 =L_X_1[1:]
+                d_0 = L_a_0 *X_1[1:]*(1-X_1[1:])
+                L_W_0=d_0.reshape(d_0.shape[0],1) @X_0.reshape(1,-1)
+                W[0] += -c*L_W_0
+                
+                
+            print("Training Accuracy:",correct/60000)
+            correct = 0
+        return (W[0],W[1])
+    def evaluate(self,W0,W1):
+            test_obs_num = self.test_X.shape[0]
+            test_X = self.test_X.reshape(test_obs_num, -1, 1)
+            test_y = np.eye(10)[self.test_y]
 
-            L_a_1 =L_X_2[1:]
-            d_1 = L_a_1 *X_2[1:]*(1-X_2[1:])
-            L_W_1=d_1.reshape(d_1.shape[0],1) @X_1.reshape(1,X_1.shape[0])
-            W_1 += -c*L_W_1
-        print("Prawidlowe:",prawidlowe/60000)
-        prawidlowe=0
-    return (W_1,W_2)
+            correct = 0
+            for i in range(test_obs_num):
+                X_0 = np.insert(test_X[i], 0, 1)
+                net_0 = W0 @ X_0
 
-ret = learn(W_1,W_2,X)
-W_1=ret[0]
-W_2=ret[1]
+                X_1 = 1 / (1 + np.exp(-net_0))
+                X_1 = np.insert(X_1, 0, 1)
+                net_1 = W1 @ X_1
+                a = 1 / (1 + np.exp(-net_1))
+                correct += int(np.argmax(a) == np.argmax(test_y[i]))
 
-with open('W_1.pkl', 'wb') as f:
-    pickle.dump(W_1, f)
+            accuracy = correct / test_obs_num
+            print("Test Accuracy:", accuracy)
 
-# Zapisz W_2 do pliku pickle
-with open('W_2.pkl', 'wb') as f:
-    pickle.dump(W_2, f)
+test = NeuralNetwork()
+W0,W1=test.fit()
+test.evaluate(W0,W1)
     
-while True:
-    ask = input("kontynuowac uczenie? T/N ")
-    if ask == "T":
-        ret = learn(W_1,W_2,X)
-        W_1=ret[0]
-        W_2=ret[1]
-
-        with open('W_1.pkl', 'wb') as f:
-            pickle.dump(W_1, f)
-
-        # Zapisz W_2 do pliku pickle
-        with open('W_2.pkl', 'wb') as f:
-            pickle.dump(W_2, f)
-    
-    try:
-        index = int(input("Liczba 0-59999 "))
-    except:
-        next
-    img = train_X[index]
-    pyplot.imshow(img.reshape(28,28),cmap="Greys")
-
-    X_1 = np.insert(X[index],0,1)
-    net_1 = W_1@X_1
-    X_2 = fun(net_1)
-    X_2 = np.insert(X_2,0,1)
-    net_2 = W_2@X_2
-    a = fun(net_2)
-    print(np.argmax(a),train_y[index])
-    pyplot.show()
-        
-        
